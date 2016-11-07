1568beb69ca4ea4e5f15fc07e2ca45155cdb0470
Bai Yan <dingdingbai0424@gmail.com>
2016-11-07 13:18:06 +0800

    修改游戏重新开始机制

--- js/main.js
+++ js/main.js
@@ -7,208 +7,219 @@ for(var i = 0; i < width * height; i++) {
   container.appendChild(div)
 }
 
-var snake = {
-  positions: [{x: 1, y: 1}],
-  direction: "R",
-  fruit: {x: 3, y: 4},
+var snake
+var interval
+var gameIsRunning = false
+
+function getInitSnake() {
+ return {
+    positions: [{x: 1, y: 1}],
+    direction: "R",
+    fruit: {x: getRandomX(), y: getRandomY()},
+  }
+}
+
+function getRandomX() {
+  return Math.floor(Math.random() * width)
+}
+
+function getRandomY() {
+  return Math.floor(Math.random() * height)
+}
+
+function getSquareByPos(pos) {
+  var index = pos.y * width + pos.x
+  return document.querySelectorAll("#container div")[index]
+}
+
+function drawSnakeAndFruit() {
+  var divs = document.querySelectorAll("#container div")
+  for(var i = 0; i < divs.length; i++) {
+    divs[i].className = ""
+  }
+
+  snake.positions.forEach(function(pos) {
+    getSquareByPos(pos).className = "snake"})
+    getSquareByPos(snake.positions[0]).className = "snakehead"
+    getSquareByPos(snake.fruit).className = "fruit"
+}
+
+var h1 = document.getElementById("score")
+
+function updateScore() {
+  var score = (snake.positions.length - 1) * 10
+  h1.innerText = "Score:" + score
+  $(".alert-content").find("strong").text(score)
+}
+
+function mainLoop() {
+  var snakeHead = snake.positions[0]
+  if(snakeHead.x === snake.fruit.x && snakeHead.y === snake.fruit.y) {
+    var lastBody = snake.positions[snake.positions.length - 1]
+    var newBody = {}
+    if(snake.direction === "U") {
+      newBody.y =  lastBody.y + 1
+      newBody.x = lastBody.x
+    }
+    if(snake.direction === "D") {
+      newBody.y = lastBody.y - 1
+      newBody.x = lastBody.x
+    }
+    if(snake.direction === "L") {
+      newBody.x =  lastBody.x + 1
+      newBody.y = lastBody.y
+    }
+    if(snake.direction === "R") {
+      newBody.x =  lastBody.x - 1
+      newBody.y = lastBody.y
+    }
+
+    snake.positions.push(newBody)
+    snake.fruit.x = getRandomX()
+    snake.fruit.y = getRandomY()
+  }
+
+  var lastPosX = snake.positions[0].x
+  var lastPosY = snake.positions[0].y
+  if(snake.direction === "L") {
+    snake.positions[0].x--
+    snake.positions.slice(1).forEach(function(pos) {
+      var a = pos.x
+      var b = pos.y
+      pos.x = lastPosX
+      pos.y = lastPosY
+      lastPosX = a
+      lastPosY = b
+    })
+  }
+  if(snake.direction === "R") {
+    snake.positions[0].x++
+    snake.positions.slice(1).forEach(function(pos) {
+      var a = pos.x
+      var b = pos.y
+      pos.x = lastPosX
+      pos.y = lastPosY
+      lastPosX = a
+      lastPosY = b
+    })
+  }
+  if(snake.direction === "U") {
+    snake.positions[0].y--
+    snake.positions.slice(1).forEach(function(pos) {
+      var a = pos.x
+      var b = pos.y
+      pos.x = lastPosX
+      pos.y = lastPosY
+      lastPosX = a
+      lastPosY = b
+    })
+  }
+  if(snake.direction === "D") {
+    snake.positions[0].y++
+    snake.positions.slice(1).forEach(function(pos) {
+      var a = pos.x
+      var b = pos.y
+      pos.x = lastPosX
+      pos.y = lastPosY
+      lastPosX = a
+      lastPosY = b
+    })
+  }
+
+  var isDead = false
+  if(snakeHead.x < 0 || snakeHead.x >= width) {
+    isDead = true
+  }
+  if(snakeHead.y < 0 || snakeHead.y >= height) {
+    isDead = true
+  }
+  snake.positions.slice(1).forEach(function(pos) {
+    if(pos.x === snakeHead.x && pos.y === snakeHead.y) {
+      isDead = true
+    }
+  })
+  if(isDead) {
+    gameIsRunning = false
+    clearInterval(interval)
+    h1.innerText += " Game Over"
+    $(".alert").css("display","flex")
+    $(".alertBoard").css("display","block")
+  } else {
+    drawSnakeAndFruit()
+    updateScore()
+  }
 }
 
 document.addEventListener("keydown", function(evt) {
-  if(evt.keyCode === 32) { 
-    function getSquareByPos(pos) {
-      var index = pos.y * width + pos.x
-      return document.querySelectorAll("#container div")[index]
+  // space, start the game
+  if(evt.keyCode === 32) {
+    if(!gameIsRunning) {
+      startGame()
     }
-
-  function drawSnakeAndFruit() {
-      var divs = document.querySelectorAll("#container div")
-      for(var i = 0; i < divs.length; i++) {
-        divs[i].className = ""
-      }
-
-    snake.positions.forEach(function(pos) {
-      getSquareByPos(pos).className = "snake"})
-      getSquareByPos(snake.positions[0]).className = "snakehead"
-      getSquareByPos(snake.fruit).className = "fruit"
-    }
-
-    var h1 = document.getElementById("score")
-
-  function updateScore() {
-      var score = (snake.positions.length - 1) * 10
-      h1.innerText = "Score:" + score
-      $(".alert-content").find("strong").text(score)
-    }
-
-    function mainLoop() {
-      var snakeHead = snake.positions[0]
-      if(snakeHead.x === snake.fruit.x && snakeHead.y === snake.fruit.y) {
-        var lastBody = snake.positions[snake.positions.length - 1]
-        var newBody = {}
-        if(snake.direction === "U") {
-          newBody.y =  lastBody.y + 1
-          newBody.x = lastBody.x
-        }
-        if(snake.direction === "D") {
-          newBody.y = lastBody.y - 1
-          newBody.x = lastBody.x
-        }
-        if(snake.direction === "L") {
-          newBody.x =  lastBody.x + 1
-          newBody.y = lastBody.y
-        }
-        if(snake.direction === "R") {
-          newBody.x =  lastBody.x - 1
-          newBody.y = lastBody.y
-        }
-        
-        snake.positions.push(newBody)
-        snake.fruit.x = Math.floor(Math.random() * width)
-        snake.fruit.y = Math.floor(Math.random() * height)
-      }
-
-      var lastPosX = snake.positions[0].x
-      var lastPosY = snake.positions[0].y
-      if(snake.direction === "L") {
-        snake.positions[0].x--
-        snake.positions.slice(1).forEach(function(pos) {
-          var a = pos.x
-          var b = pos.y
-          pos.x = lastPosX
-          pos.y = lastPosY
-          lastPosX = a
-          lastPosY = b
-        })
-      }
-      if(snake.direction === "R") {
-        snake.positions[0].x++
-        snake.positions.slice(1).forEach(function(pos) {
-          var a = pos.x
-          var b = pos.y
-          pos.x = lastPosX
-          pos.y = lastPosY
-          lastPosX = a
-          lastPosY = b
-        })
-      }
-      if(snake.direction === "U") {
-        snake.positions[0].y--
-        snake.positions.slice(1).forEach(function(pos) {
-          var a = pos.x
-          var b = pos.y
-          pos.x = lastPosX
-          pos.y = lastPosY
-          lastPosX = a
-          lastPosY = b
-        })
-      }
-      if(snake.direction === "D") {
-        snake.positions[0].y++
-        snake.positions.slice(1).forEach(function(pos) {
-          var a = pos.x
-          var b = pos.y
-          pos.x = lastPosX
-          pos.y = lastPosY
-          lastPosX = a
-          lastPosY = b
-        })
-      }
-
-      var isDead = false
-      if(snakeHead.x < 0 || snakeHead.x >= width) {
-        isDead = true
-      }
-      if(snakeHead.y < 0 || snakeHead.y >= height) {
-        isDead = true
-      }
-      snake.positions.slice(1).forEach(function(pos) {
-        if(pos.x === snakeHead.x && pos.y === snakeHead.y) {
-          isDead = true
-        }
-      })
-      if(isDead) {
-        clearInterval(interval)
-        h1.innerText += " Game Over"
-        $(".alert").css("display","flex")
-        $(".alertBoard").css("display","block")
-        return
-      } else {
-        drawSnakeAndFruit()
-        updateScore()
-      }
-    }
-
-    document.addEventListener("keydown", function(evt) {
-      // up
-      if(evt.keyCode === 38) {
-        var newPosX = snake.positions[0].x
-        var newPosY = snake.positions[0].y - 1
-        var isValid = true
-        snake.positions.slice(1).forEach(function(pos) {
-          if(pos.x === newPosX && pos.y === newPosY) {
-            isValid = false
-          }
-        })
-        if(isValid) {
-          snake.direction = "U"
-        }
-      }
-      // down
-      if(evt.keyCode === 40) {
-        var newPosX = snake.positions[0].x
-        var newPosY = snake.positions[0].y + 1
-        var isValid = true
-        snake.positions.slice(1).forEach(function(pos) {
-          if(pos.x === newPosX && pos.y === newPosY) {
-            isValid = false
-          }
-        })
-        if(isValid) {
-          snake.direction = "D"
-        }
-      }
-      // left
-      if(evt.keyCode === 37) {
-        var newPosX = snake.positions[0].x - 1
-        var newPosY = snake.positions[0].y
-        var isValid = true
-        snake.positions.slice(1).forEach(function(pos) {
-          if(pos.x === newPosX && pos.y === newPosY) {
-            isValid = false
-          }
-        })
-        if(isValid) {
-          snake.direction = "L"
-        }
-      }
-      // right
-      if(evt.keyCode === 39) {
-        var newPosX = snake.positions[0].x + 1
-        var newPosY = snake.positions[0].y
-        var isValid = true
-        snake.positions.slice(1).forEach(function(pos) {
-          if(pos.x === newPosX && pos.y === newPosY) {
-            isValid = false
-          }
-        })
-        if(isValid) {
-          snake.direction = "R"
-        }
+  }
+  // up
+  if(evt.keyCode === 38) {
+    var newPosX = snake.positions[0].x
+    var newPosY = snake.positions[0].y - 1
+    var isValid = true
+    snake.positions.slice(1).forEach(function(pos) {
+      if(pos.x === newPosX && pos.y === newPosY) {
+        isValid = false
       }
     })
-
-  updateScore()
-  var interval = setInterval(mainLoop, 150)
+    if(isValid) {
+      snake.direction = "U"
+    }
   }
-
-$(".confirm").click(function(){
-  h1.innerText = "Score:" + 0
-  $(".alert").css("display","none")
-  $(".alertBoard").css("display","none")
-  $("#container > div").removeClass("snakehead")
-  $("#container > div").removeClass("fruit")
-  $("#container > div").removeClass("newBody")
+  // down
+  if(evt.keyCode === 40) {
+    var newPosX = snake.positions[0].x
+    var newPosY = snake.positions[0].y + 1
+    var isValid = true
+    snake.positions.slice(1).forEach(function(pos) {
+      if(pos.x === newPosX && pos.y === newPosY) {
+        isValid = false
+      }
+    })
+    if(isValid) {
+      snake.direction = "D"
+    }
+  }
+  // left
+  if(evt.keyCode === 37) {
+    var newPosX = snake.positions[0].x - 1
+    var newPosY = snake.positions[0].y
+    var isValid = true
+    snake.positions.slice(1).forEach(function(pos) {
+      if(pos.x === newPosX && pos.y === newPosY) {
+        isValid = false
+      }
+    })
+    if(isValid) {
+      snake.direction = "L"
+    }
+  }
+  // right
+  if(evt.keyCode === 39) {
+    var newPosX = snake.positions[0].x + 1
+    var newPosY = snake.positions[0].y
+    var isValid = true
+    snake.positions.slice(1).forEach(function(pos) {
+      if(pos.x === newPosX && pos.y === newPosY) {
+        isValid = false
+      }
+    })
+    if(isValid) {
+      snake.direction = "R"
+    }
+  }
 })
 
-})
-
+function startGame() {
+  gameIsRunning = true
+  $(".alert").hide()
+  $(".alertBoard").hide()
+  snake = getInitSnake()
+  updateScore()
+  interval = setInterval(mainLoop, 150)
+}
